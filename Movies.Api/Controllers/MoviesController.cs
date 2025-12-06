5e172fa8b68eb337b1c0452c8055e3de7f9a42f8
niou-dev <hughmungusgr@gmail.com>
2025-12-06 18:09:58 +0200

    Add project files.

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        // constructor Dependency Injection
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost(ApiEndpoints.Movies.Create)]
+        public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+        {
+            var movie = request.MapToMovie();
+            await _movieRepository.CreateAsync(movie);
+            // rest api should not return Ok at a creation
+            // return Ok(movie);
+
+            // it should return this
+            //return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie.MapToResponse());
+
+            return CreatedAtAction(nameof(Get), new {id = movie.Id}, movie.MapToResponse());
+        }
+
+        [HttpGet(ApiEndpoints.Movies.Get)]
+        public async Task<IActionResult>Get([FromRoute] Guid id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            // WRONG, YOU SHOULD RETURN CONTRACT NOT YOUR DOMAIN MODEL!
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+
+        [HttpGet(ApiEndpoints.Movies.GetAll)]
+        public async Task<IActionResult> GetAll()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+
+            var moviesResponse = movies.MapToResponse();
+            return Ok(moviesResponse);
+        }
+
+        [HttpPut(ApiEndpoints.Movies.Update)]
+        public async Task<IActionResult> Update([FromRoute]Guid id, 
+            [FromBody]UpdateMovieRequest request)
+        {
+            var movie = request.MapToMovie(id);
+
+            var updated = await _movieRepository.UpdateAsync(movie);
+
+            if (!updated)
+            {
+                return NotFound();
+            }
+
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+
+        [HttpDelete(ApiEndpoints.Movies.Delete)]
+        public async Task<IActionResult> Delete([FromRoute]Guid id)
+        {
+            var deleted = await _movieRepository.DeleteByIdAsync(id);
+
+            if (!deleted)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ Movies.Api/Program.cs
@@ -0,0 +1,39 @@
+using Movies.Application;
+
+namespace Movies.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            //builder.Services.AddEndpointsApiExplorer();
+            // builder.Services.AddSwaggerGen();
+            // skaei apo to application layer mou logw tou package me to abstraction
+            builder.Services.AddApplication();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
